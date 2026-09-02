4dfcb61a849a036ce9ad5d6cdfdef9f33a145293
SSMMILEE <grover.js39@gmail.com>
2026-09-02 18:39:07 -0400

    fix: import android.util.Log in MainActivity.kt

--- app/src/main/java/com/iamax/launcher/MainActivity.kt
+++ app/src/main/java/com/iamax/launcher/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.view.View
 import android.webkit.CookieManager
 import android.webkit.MimeTypeMap
